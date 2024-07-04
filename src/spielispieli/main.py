ad4bb37e3ef83cb8bf555db428999c6d40d713fe
Thomas Thron <throni3git@gmail.com>
2024-07-04 23:05:07 +0200

    refactor

--- src/spielispieli/main.py
+++ src/spielispieli/main.py
@@ -1,6 +1,4 @@
-import sys
 from pathlib import Path
-from typing import Dict, List, Optional
 import logging
 import time
 
@@ -13,26 +11,25 @@ from spielispieli.utils import improved_sleep
 log = logging.getLogger(__name__)
 
 
-
-def SPIELISPIELI_LOOP():
-
+def init_game() -> None:
     pygame.init()
     pygame.display.set_caption("DasRender keyboard input")
     icon = pygame.image.load(Path(__file__).parent / "resources/polyggle.png")
     pygame.display.set_icon(icon)
 
-    size = (256, 256)
+def init_screen(size: tuple[int, int]) -> tuple[pygame.Surface, pygame.Surface]:
+
     screen = pygame.display.set_mode(size)
     background = pygame.image.load(Path(__file__).parent / "resources/polyggle.png")
     background.set_alpha(int(0.2*256))
+    return screen, background
 
-    font_size = 20
-    font = pygame.font.Font('freesansbold.ttf', font_size)
-    dbg_font = pygame.font.Font('freesansbold.ttf', 10)
-    font_instructions = pygame.font.Font('freesansbold.ttf', 9)
+def run_loop(surface_screen: pygame.Surface, 
+                assets: dict[str, pygame.Surface], 
+                fonts: dict[str, pygame.font.Font]) -> None:
 
-    _held_keys = []
-    _held_inputs = {}
+    held_keys = []
+    held_inputs = {}
     player_pos = Vec2(0, 0)
     velocity = 100
     FPS = 60
@@ -53,46 +50,64 @@ def SPIELISPIELI_LOOP():
                 running = False
             if event.type == pygame.KEYDOWN:
                 key_name = pygame.key.name(event.key)
-                _held_keys.append(key_name)
-                _held_inputs[f"{key_name}_held"] = 1.0
+                held_keys.append(key_name)
+                held_inputs[f"{key_name}_held"] = 1.0
             if event.type == pygame.KEYUP:
                 key_name = pygame.key.name(event.key)
-                _held_keys.remove(key_name)
+                held_keys.remove(key_name)
                 inputs[f"{key_name}_up"] = 1.0
 
                 held_key_name = f"{key_name}_held"
-                if held_key_name in _held_inputs:
-                    del _held_inputs[held_key_name]
+                if held_key_name in held_inputs:
+                    del held_inputs[held_key_name]
 
         delta = time_passed * velocity
-        if "w" in _held_keys:
+        if "w" in held_keys:
             player_pos.y += delta
-        if "s" in _held_keys:
+        if "s" in held_keys:
             player_pos.y -= delta
-        if "d" in _held_keys:
+        if "d" in held_keys:
             player_pos.x += delta
-        if "a" in _held_keys:
+        if "a" in held_keys:
             player_pos.x -= delta
 
-        screen.fill((44, 44, 44))
-        screen.blit(background, (64, 64))
+        surface_screen.fill((44, 44, 44))
+        surface_screen.blit(assets["background"], (64, 64))
 
-        text = font.render(", ".join(_held_keys), True, (255, 255, 255))
-        screen.blit(text, (5, 5))
+        text = fonts["big"].render(", ".join(held_keys), True, (255, 255, 255))
+        surface_screen.blit(text, (5, 5))
 
-        text = font.render("+", True, (255, 255, 255))
+        text = fonts["big"].render("+", True, (255, 255, 255))
         aaaaarg = 2*np.pi * blink_velocity * time_since_start_of_program
         alpha = (np.sin(aaaaarg)*0.5+0.5) * 256
         text.set_alpha(int(alpha))
-        screen.blit(text, ((player_pos.x), size[1] - font_size - int(player_pos.y)))
+        pos = (player_pos.x+3, surface_screen.get_size()[1] - fonts["big"].get_linesize() - int(player_pos.y))
+        surface_screen.blit(text, pos)
 
-        text = dbg_font.render(f"{time_since_start_of_program:.2f}", True, (255, 255, 255))
-        screen.blit(text, (size[0] - text.get_width()-5, 5))
+        text = fonts["default"].render(f"{time_since_start_of_program:.2f}", True, (255, 255, 255))
+        surface_screen.blit(text, (surface_screen.get_size()[0] - text.get_width()-5, 5))
 
         pygame.display.update()
 
         improved_sleep(1/FPS)
 
+def main():
+    init_game()
+
+    screen_size = (256, 256)
+    surface_screen, surface_background = init_screen(screen_size)
+    
+    assets = {
+        "background": surface_background,
+    }
+    fonts= {
+        "big" : pygame.font.Font('freesansbold.ttf', 20),
+        "default" : pygame.font.Font('freesansbold.ttf', 10)
+    }
+
+    run_loop(surface_screen, assets, fonts)
+
+
 
 if __name__ == "__main__":
-    SPIELISPIELI_LOOP()
+    main()
